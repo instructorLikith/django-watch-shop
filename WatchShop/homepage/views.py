30b4356d154336ec97dfa3c32b18c3742f52e723
instructorLikith <instructorLikith@gmail.com>
2024-09-09 22:09:13 +0530

    Django jinja templating

--- WatchShop/homepage/views.py
+++ WatchShop/homepage/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse    
+from django.shortcuts import render 
+from .models import Watches
 
 # Create your views here.
 def Home(request):
-    return render(request, 'home.html')
+    watches = Watches.objects.all()
+    context = {'watches_t': watches}
+    return render(request, 'home.html', context)
 
 def About(request):
     return render(request, 'about.html')
