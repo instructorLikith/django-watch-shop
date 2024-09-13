1db8d39623f78705ae9f6b89b453e5a76900da70
instructorLikith <instructorLikith@gmail.com>
2024-09-13 21:11:01 +0530

    Django user Authentication

--- WatchShop/homepage/views.py
+++ WatchShop/homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from .models import Watches
-from .forms import UploadForm       
+from .forms import UploadForm 
+from django.contrib.auth.decorators import login_required      
 
 # Create your views here.
 def Home(request):
@@ -11,7 +12,7 @@ def Home(request):
 def About(request):
     return render(request, 'about.html')
 
-
+@login_required
 def Upload(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
@@ -21,5 +22,50 @@ def Upload(request):
     else:
         form = UploadForm()
         
-
     return render(request,'WatchUpload.html', {'form': form})
+
+
+# LOGIN
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, logout, login
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            user_name = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username = user_name, password = password)
+            
+            if user is not None:
+                 login(request,user)
+                 return redirect('home') 
+            else:
+                return render(request, 'login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        
+    return render(request,'login.html', {'form': form})
+    
+   
+#SIGNUP
+def signup_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+#LOGOUT
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
